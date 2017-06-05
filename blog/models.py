7df3b36377bdd955fd8ba459293d661099ece483
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-05 23:13:24 +0200

    Add taggit and first model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    pub_data = models.DateTimeField(verbose_name="Data publikacji")
+    tags = TaggableManager()
+    content = models.TextField()
