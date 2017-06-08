c1cf93129ad0cbd37ba709ea5a35c67b2a925048
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-08 21:26:19 +0200

    Add Markdown support

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from mistune import markdown
 
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.compiled_content = markdown(obj.content)
+        obj.save()
+
+admin.site.register(Post, PostAdmin)
