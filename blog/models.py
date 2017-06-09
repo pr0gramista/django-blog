41d84cdede3d4f24af0e0c73da58ad6288b30f82
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-09 21:48:12 +0200

    Remove old icons and add published property to post

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Tytuł")
     slug = models.SlugField(default='')
     pub_date = models.DateTimeField(verbose_name="Data publikacji")
     raw_content = models.TextField(verbose_name="Zawartość surowa")
-    content = models.TextField(verbose_name="Zawartość", default='')
+    content = models.TextField(verbose_name="Zawartość", default='', blank=True)
+    published = models.BooleanField(verbose_name="Opublikowany", default=False)
     tags = TaggableManager()
 
 class Page(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Post, Page
 
 def index(request):
-    latest_posts = Post.objects.order_by('-pub_date')[:5]
+    latest_posts = Post.objects.filter(published=True).order_by('-pub_date')[:5]
     context = {'posts': latest_posts}
     return render(request, 'blog/index.html', context)
 
 def post(request, post_slug):
-    post = Post.objects.get(slug=post_slug)
+    query = Post.objects.filter(published=True)
+    post = get_object_or_404(query, slug=post_slug)
     context = {'post': post}
     return render(request, 'blog/post.html', context)
 
