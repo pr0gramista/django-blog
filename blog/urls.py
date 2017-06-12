7a8a676e2b292924901136b3507e78fd5fb1b873
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-12 19:59:31 +0200

    Add tag view

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post/(?P<post_slug>[aA-zZ0-9-]+)/$', views.post, name='post'),
-    url(r'^page/(?P<page_slug>[aA-zZ0-9-]+)/$', views.page, name='page')
+    url(r'^page/(?P<page_slug>[aA-zZ0-9-]+)/$', views.page, name='page'),
+    url(r'^tag/(?P<tag_slug>[aA-zZ0-9-]+)/$', views.tag, name='tag')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from taggit.models import Tag
 
 from .models import Post, Page
 
@@ -13,6 +14,12 @@ def post(request, post_slug):
     post = get_object_or_404(query, slug=post_slug)
     context = {'post': post}
     return render(request, 'blog/post.html', context)
+
+def tag(request, tag_slug):
+    posts = Post.objects.filter(published=True).filter(tags__slug__in=[tag_slug]).all()
+    tag = Tag.objects.get(slug=tag_slug)
+    context = {'posts': posts, 'tag': tag}
+    return render(request, 'blog/tag.html', context)
 
 def page(request, page_slug):
     page = Page.objects.get(slug=page_slug)
