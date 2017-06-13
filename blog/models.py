8df7632ba76656fd56f693a5d3995dcb267188bb
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-13 03:30:24 +0200

    Add new index post widget

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from taggit.managers import TaggableManager
 from imagekit.models import ImageSpecField, ProcessedImageField
-from imagekit.processors import ResizeToFit
+from imagekit.processors import ResizeToFit, ResizeToCover
 
 class Post(models.Model):
+    COVER = 'cover'
+    CONTAIN = 'contain'
+    IMAGE_STATEGY = {
+        (COVER, 'cover'),
+        (CONTAIN, 'contain')
+    }
+
     title = models.CharField(max_length=200, verbose_name="Tytuł")
     slug = models.SlugField(default='')
+    show_title = models.BooleanField(verbose_name="Pokaż tytuł", default=True)
+    image = ProcessedImageField(
+        upload_to='images/posts',
+        default='',
+        verbose_name="Główny obrazek",
+        processors=[ResizeToCover(800, 600)],
+        format='JPEG',
+        options={'quality': 80})
+    image_display_stategy = models.CharField(max_length=30, choices=IMAGE_STATEGY, default=COVER)
     pub_date = models.DateTimeField(verbose_name="Data publikacji")
     raw_content = models.TextField(verbose_name="Zawartość surowa")
     content = models.TextField(verbose_name="Zawartość", default='', blank=True)
