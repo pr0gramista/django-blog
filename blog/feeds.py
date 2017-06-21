a7a1720d9f9079d1b4375ac438741b356ce65416
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-21 18:32:19 +0200

    Add RSS feeds

---
+++ blog/feeds.py
@@ -0,0 +1,52 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+from taggit.models import Tag
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = "PR0GRAMISTA"
+    link = "/rss/"
+    description = "Ostatnie posty na blogu PR0GRAMISTA.pl"
+
+    def items(self):
+        return Post.objects.order_by('-pub_date').all()
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
+    def item_pubdate(self, item):
+        return item.pub_date
+
+    def item_link(self, item):
+        return reverse('post', args=[item.slug])
+
+
+class TagPostsFeed(Feed):
+
+    def get_object(self, request, tag_slug):
+        return Tag.objects.get(slug=tag_slug)
+
+    def items(self, obj):
+        return Post.objects.filter(published=True).filter(tags__slug__in=[obj.slug]).order_by('-pub_date').all()
+
+    def link(self, obj):
+        return "/tag/%s/rss" % obj.slug
+
+    def title(self, obj):
+        return "%s | PR0GRAMISTA" % obj.name
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
+    def item_pubdate(self, item):
+        return item.pub_date
+
+    def item_link(self, item):
+        return reverse('post', args=[item.slug])
--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+from . import views, feeds
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -10,5 +10,7 @@ urlpatterns = [
     url(r'^post/(?P<post_slug>[aA-zZ0-9-]+)/$', views.post, name='post'),
     url(r'^page/(?P<page_slug>[aA-zZ0-9-]+)/$', views.page, name='page'),
     url(r'^tag/(?P<tag_slug>[aA-zZ0-9-]+)/(?P<pagination>[1-9]+)/$', views.tag_pagination, name='tag_pagination'),
-    url(r'^tag/(?P<tag_slug>[aA-zZ0-9-]+)/$', views.tag, name='tag')
+    url(r'^tag/(?P<tag_slug>[aA-zZ0-9-]+)/$', views.tag, name='tag'),
+    url(r'^rss/$', feeds.LatestPostsFeed(), name='rss_index'),
+    url(r'^tag/(?P<tag_slug>[aA-zZ0-9-]+)/rss/$', feeds.TagPostsFeed(), name='rss_tag'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
