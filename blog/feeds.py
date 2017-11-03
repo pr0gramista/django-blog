ffae8935186237d1c0d83cdabd49b925c413edf0
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-11-03 17:12:31 +0100

    Add encoding declaration for Python 2

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,3 +1,4 @@
+# coding=utf8
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
 from taggit.models import Tag
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# coding=utf8
 from django.db import models
 from taggit.managers import TaggableManager
 from imagekit.models import ImageSpecField, ProcessedImageField
