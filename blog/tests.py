d8be39ccf93840eaef61760d6c97b321191ce118
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-11-03 16:47:38 +0100

    Add mistune (markdown) tests

--- blog/tests.py
+++ blog/tests.py
@@ -3,7 +3,10 @@ from django.urls import reverse
 from django.utils import timezone
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth.models import User
+from mistune import Markdown
 from .models import Post, HeaderImage
+from .markdown import PostRenderer, PostInlineLexer
+
 
 example_image = SimpleUploadedFile(name='test.png', content=open(
     'test.png', 'rb').read(), content_type='image/jpeg')
@@ -125,3 +128,65 @@ class PostViewTests(TestCase):
 
         response = self.client.get(reverse('post', args=post.slug))
         self.assertEqual(response.status_code, 200)
+
+
+class MarkdownEditorTests(TestCase):
+    def __init__(self, methodName):
+        super().__init__(methodName)
+        renderer = PostRenderer()
+        inline = PostInlineLexer(renderer)
+        inline.enable_woo()
+        inline.enable_emdash()
+        inline.enable_figure()
+
+        self.markdown = Markdown(renderer, inline=inline, escape=False)
+
+    def test_standard_markdown_tags(self):
+        test_string = ("# h1\n"
+                       "## h2\n"
+                       "### h3\n"
+                       "#### h4\n"
+                       "##### h5\n"
+                       "###### h6\n"
+                       "*wow* **such markdown**\n"
+                       "\n"
+                       "> doge: markdown such intelligent\n"
+                       "\n"
+                       "[this is link](https://pr0gramista.pl)")
+        output = self.markdown.render(test_string)
+
+        correct_output = ("<h1>h1</h1>\n"
+                          "<h2>h2</h2>\n"
+                          "<h3>h3</h3>\n"
+                          "<h4>h4</h4>\n"
+                          "<h5>h5</h5>\n"
+                          "<h6>h6</h6>\n"
+                          "<p><em>wow</em> <strong>such markdown</strong></p>\n"
+                          "<blockquote><p>doge: markdown such intelligent</p>\n"
+                          "</blockquote>\n"
+                          "<p><a href=\"https://pr0gramista.pl\">this is link</a></p>\n")
+
+        self.assertHTMLEqual(output, correct_output)
+
+    def test_custom_markdown_tags(self):
+        test_string = ("This !-- is endash\n"
+                       "![This is image](http://via.placeholder.com/350x150)\n"
+                       "![This is a figure][This is a figure alt](http://via.placeholder.com/450x300)\n"
+                       "!F[This is woo][done][FF0000][](https://docs.djangoproject.com/en/1.11/topics/testing/tools/)")
+        output = self.markdown.render(test_string)
+
+        correct_output = ("<p>This — is endash\n"
+                          "<img src=\"http://via.placeholder.com/350x150\" alt=\"This is image\">\n"
+                          "<figure><img src=\"http://via.placeholder.com/450x300\" alt=\"This is a figure alt\" /><div>This is a figure</div></figure>\n"
+                          "\n"
+                          "<div class=\"woo\">\n"
+                          "  <div class=\"woo-fake\" style=\"background: #FF0000;\"></div>\n"
+                          "  <a href=\"https://docs.djangoproject.com/en/1.11/topics/testing/tools/\"><div class=\"woo-content-wrapper\">\n"
+                          "    <div class=\"woo-content \">\n"
+                          "      <i class=\"material-icons\">done</i>\n"
+                          "      <div>This is woo</div>\n"
+                          "    </div>\n"
+                          "  </div></a>\n"
+                          "</div>")
+
+        self.assertHTMLEqual(output, correct_output)
